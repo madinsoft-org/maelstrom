6e62a26b2e50f3c5ccac7e087dc0cabf1adfa6f7
patrick <madinsoft@gmail.com>
2026-04-08 20:25:20 +0200

    [deploy] test deploiement with traefik

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  // Dis à Next.js que toutes les URLs doivent commencer par /maelstrom
+  basePath: '/maelstrom',
+
+  // Optionnel mais conseillé si tu as des images dans /public
+  assetPrefix: '/maelstrom',
 };
 
 export default nextConfig;
